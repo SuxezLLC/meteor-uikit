92ce8c3405e62bcc511377497e855d9f5ca70eeb
Roman Geber <rgeber@suxez.com>
2016-01-10 13:33:01 +0100

    Specified fontawesome version.

--- package.js
+++ package.js
@@ -13,9 +13,9 @@ Package.describe({
 Package.onUse(function (api) {
     api.versionsFrom('1.1.0.2');
 
-    api.use(['fortawesome:fontawesome']);
+    api.use(['fortawesome:fontawesome@4.5.0']);
 
-    api.imply(['fortawesome:fontawesome']);
+    api.imply(['fortawesome:fontawesome@4.5.0']);
 
     // Add assets
     api.addFiles([
